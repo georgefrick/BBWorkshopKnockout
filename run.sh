6b67ef1edc86e8bb0ff9891ceb427674a71eef9c
Jeff Gitter <gitter1226@gmail.com>
2014-01-14 12:38:55 -0600

    Checking for db files

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# install node dependencies
 sudo npm install
+
+# bootstrap the database if it doesn't exist
+[ ! -f server/restaurants.db ] && ./reset.sh
+
+# precompile the handlebars templates
 ./node_modules/handlebars/bin/handlebars webapp/template/*.handlebars -f webapp/js/precompiled-templates.js
+
+# startup the node server
 node server/server --port 9000
