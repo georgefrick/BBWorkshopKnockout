918fd01f584735f6c1eb5f7eb3834c446ce74f17
Jeff Gitter <gitter1226@gmail.com>
2014-01-14 12:25:52 -0600

    Reverting run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 echo off
 call npm install
-call node server/bootstrap
+rem This was changed to call node directly, as the bin file isn't a windows executable.
+call node node_modules/handlebars/bin/handlebars webapp/template -f webapp/js/precompiled-templates.js
+call node server/server --port 9000
